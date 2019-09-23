f3000fcb56c082eaf340ea9950b6a9994f26fe2f
ar7076 <Aravind.Chandrasekara@dal.ca>
2019-09-23 11:54:42 +0000

    first commit

---
+++ rotatepairs.py
@@ -0,0 +1,6 @@
+myword = open("https://www.gutenberg.org/files/2600/2600-0.txt")
+def rotate_pairs( letter,s)
+         word = myword.strip()
+         words = word.split()
+      for letter in s:
+         
